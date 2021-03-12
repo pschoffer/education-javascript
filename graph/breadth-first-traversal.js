6a4ec71afea4e10ecb234dfdc091d7fd033841ab
fridavbg <fridavbg@gmail.com>
2021-03-12 19:13:19 +0100

    iterative approach

--- graph/breadth-first-traversal.js
+++ graph/breadth-first-traversal.js
@@ -1,18 +1,54 @@
+
+// Iterative approach
 function breadthFirstTraversal(graph, root) {
-    return [];
+    
+    // QUEUE - FIFO - BFT
+    //      [  ]  a
+    //      [ a  ] b   
+    //   <- [ a b ] 
+    // a <- [ b  ]  
+
+    // STACK - LIFO
+    // [   ] a
+    // [ a  ] b
+    // [ a b  ] ->
+    // [ a  ] b
+
+    let queue = [];
+
+    queue.push(root);
+    // queue = [ a ]
+
+    const nodes = [];
+
+    while ( queue.length ) {
+        const currentNode = queue.shift();
+        // ACTION 
+        nodes.push(currentNode);
+        /*
+        when I say explode, I mean just the part that generates kids for parent not 
+        processing them yet (which explore implies also procesing a bit to me).  
+        */
+        // EXPLODE (Children)
+        const children = graph[currentNode];
+        queue = queue.concat(children);
+
+        console.log("My Test", currentNode, '=>', children, "QUEUE", queue);
+
+    }
+
+    return nodes;
 }
 
 
-
-function breadthFirstTraversalRecursive(graph, currentNode, queue = []) {
+// RECURSIVE approach
+/* function breadthFirstTraversalRecursive(graph, currentNode, queue = []) {
     return [];
-}
+} */
 
 
-
-
-
+// EXPORT CHANGES 
 module.exports = {
     breadthFirstTraversal,
-    breadthFirstTraversalRecursive
+  //  breadthFirstTraversalRecursive
 }
--- graph/breadth-first-traversal.test.js
+++ graph/breadth-first-traversal.test.js
@@ -1,45 +1,57 @@
 
-//     a
-//    / \
-//   b   c
-//  / \
-// d   e
+// VISUAL GRAPH
 
-// a: [b,c],
+//     a     root
+//    / \
+//   b   c   1st level
+//  / \
+// d   e     2nd level
+
+//  1st step. traversal
+//  2nd step. action to node
+
+// VISUAL OBJECT - convert graph to an object
+
+// a: [b,c],   
 // b: [d,e],
 // c: [],
 // d: [],
 // e: [],
 
 
-// DFT: a - b - d - e - c
-// BFT: a - b - c - d - e
+// DFT: a - b - d - e - c            
 
+// BFT: a - b - c - d - e        level by level
 
-
-const { breadthFirstTraversal, breadthFirstTraversalRecursive } = require('./breadth-first-traversal')
+// IMPORT modules
+// desctruct key in order to use directly
+// breadthFirstTraversalRecursive 
+const { breadthFirstTraversal } = require('./breadth-first-traversal')
 
 test('BFT',
     () => {
+    /* 
+        use const as much as possible - to avoid mutable state
+    */    
         const graph = {
             a: ['b', 'c'],
             b: ['d', 'e'],
             c: [],
-            d: [],
+            d: [],  
             e: [],
         }
 
         const nodes = breadthFirstTraversal(graph, 'a');
 
-
         const expectedNodes = ['a', 'b', 'c', 'd', 'e'];
 
+        // less strict than toBe();
         expect(nodes).toStrictEqual(expectedNodes);
     }
 );
 
 
-test('BFT recursive',
+/* test('BFT recursive',
     () => {
         const graph = {
             a: ['b', 'c'],
@@ -50,10 +62,9 @@ test('BFT recursive',
         }
 
         const nodes = breadthFirstTraversalRecursive(graph, 'a');
-
-
+    
         const expectedNodes = ['a', 'b', 'c', 'd', 'e'];
 
         expect(nodes).toStrictEqual(expectedNodes);
     }
-);
+);  */
